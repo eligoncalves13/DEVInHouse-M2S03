1e105c2f293a1dbfc80598e634b877c24b256446
Élida <elidabeatriz13@gmail.com>
2022-03-16 02:05:16 -0300

    add ex1

---
+++ Ex 1 - Criar classe Pessoa/src/Ex1.Class1/Person.cs
@@ -0,0 +1,18 @@
+﻿namespace Ex1.Class1
+{
+    public class Person
+    {
+		private string _name;
+		private DateTime _birthDate;
+		private int _height;
+
+		public string Name { get => _name; set => _name = value; }
+		public DateTime BirthDate { get => _birthDate; set => _birthDate = value; }
+		public int Height { get => _height; set => _height = value; }
+		
+		public string RetornarDados()
+		{
+			return $"Nome: {Name}, Data de Nascimento: {BirthDate}, Altura: {Height}";
+		}
+    }
+}
